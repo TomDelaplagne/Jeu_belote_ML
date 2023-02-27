2006dc97e08331661d3eb02cfaff36c6f8f3a9e0
TomDelaplagne <delaplagnetom@gmail.com>
2023-02-27 18:08:04 +0100

    card class adjustement

--- Game/card_class.py
+++ Game/card_class.py
@@ -55,4 +55,31 @@ class Card:
         if p2 is None:
             return False
         return self.suit == p2.suit and self.rank == p2.rank
+    
+    def calculate_card_points(self, trump_suit: str) -> int:
+        """Calculate the points scored by a single card in the Belote game."""
+        non_trump = {
+            'Ace': 11,
+            '10': 10,
+            'King': 4,
+            'Queen': 3,
+            'Jack': 2,
+            '9': 0,
+            '8': 0,
+            '7': 0
+            }
+        trump = {
+            'Jack': 20,
+            '9': 14,
+            'Ace': 11,
+            '10': 10,
+            'King': 4,
+            'Queen': 3,
+            '8': 0,
+            '7': 0
+            }
+        if self.suit == trump_suit:
+            return trump[self.rank]
+        else:
+            return non_trump[self.rank]
 
